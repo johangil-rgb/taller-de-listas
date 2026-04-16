3c991aa52c8605d3b2d9fa837c580fae580bd0b3
johan sebastian gil <johangil@colegioguanenta.edu.co>
2026-04-16 17:01:11 -0500

    Add files via upload

---
+++ taller-de-listas/taller_de_listas.py
@@ -0,0 +1,12 @@
+# 1. Crear la lista con tres amigos
+mejores_amigos = ["Diego", "juan diego", "jonatan"]
+
+# 2. Agregar un nuevo amigo
+mejores_amigos.append("juan manuel")
+
+# 3. Eliminar al segundo amigo de la lista
+mejores_amigos.pop(1)
+
+# 4. Imprimir la lista final y la cantidad de amigos
+print("Lista final de mejores amigos:", mejores_amigos)
+print("Cantidad de amigos:", len(mejores_amigos))
