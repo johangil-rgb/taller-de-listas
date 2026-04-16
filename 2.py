4433adf73a3f297ed7305ebcfaaec28759675150
johan sebastian gil <johangil@colegioguanenta.edu.co>
2026-04-16 17:02:17 -0500

    Delete 2.py

--- 2.py
+++
@@ -1,9 +0,0 @@
-# tus datos
-edades = [15,15,15,15]
-musica = ['cumbias', 'electronica', 'cualquiera', 'phonk']
-
-# reto 1: promedio de edad
-promedio = sum(edades) / len('edades')
-print('Promedio de edad:', promedio)
-
-#reto 2: mayores de 15
